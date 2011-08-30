01f08e6f86644df56f157ad63b26f2910416ebe3
Henri Gourvest <hgourvest@progdigy.com>
2011-08-30 15:12:30 +0000

    firemonkey support

---
+++ demos/firemonkey/firemonkey.dpr
@@ -0,0 +1,14 @@
+program firemonkey;
+
+uses
+  FMX.Forms,
+  main in 'main.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+
+end.
